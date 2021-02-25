8184242d69cc406848294bd95754f9c995afe01a
salaheddin <salah.infospace@gmail.com>
2021-02-25 15:42:54 +0100

    Add product page

--- assets/js/carousel-appscreen1.js
+++ assets/js/carousel-appscreen1.js
@@ -1,41 +1,78 @@
-(function($){
+(function ($) {
 
 	'use strict';
 
 	var $owl = $('#section-appscreen1 .owl-carousel');
 
-	$owl.children().each( function( index ) {
-	  	$(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+	$owl.children().each(function (index) {
+		$(this).attr('data-position', index); // NB: .attr() instead of .data()
 	});
 
 	$owl.owlCarousel({
-	  	loop:true,
-	    margin:30,
-	    nav:false,
-	    center: true,
-	    dots:true,
-	    responsive:{
-	        1655:{
-	            items:5,
-	        },
-	        1200:{
-	            items:4,
-	        },
-	        768:{
-	            items:3,
-	        },
-	        576:{
-	            items:2,
-	            spaceBetween: 15,
-	        },
-	        0:{
-	            items:1,
-	        }
-	    }
+		loop: true,
+		margin: 30,
+		nav: false,
+		center: true,
+		dots: true,
+		responsive: {
+			1655: {
+				items: 5,
+			},
+			1200: {
+				items: 4,
+			},
+			768: {
+				items: 3,
+			},
+			576: {
+				items: 2,
+				spaceBetween: 15,
+			},
+			0: {
+				items: 1,
+			}
+		}
 	});
 
-	$(document).on('click', '#section-appscreen1 .owl-item>div', function() {
-	  	$owl.trigger('to.owl.carousel', $(this).data( 'position' ) );
+	$(document).on('click', '#section-appscreen1 .owl-item>div', function () {
+		$owl.trigger('to.owl.carousel', $(this).data('position'));
 	});
-	
+
+
+	var $owl = $('#section-appscreen2 .owl-carousel');
+
+	$owl.children().each(function (index) {
+		$(this).attr('data-position', index); // NB: .attr() instead of .data()
+	});
+
+	$owl.owlCarousel({
+		loop: true,
+		margin: 30,
+		nav: false,
+		center: true,
+		dots: true,
+		responsive: {
+			1655: {
+				items: 4,
+			},
+			1200: {
+				items: 3,
+			},
+			768: {
+				items: 3,
+			},
+			576: {
+				items: 2,
+				spaceBetween: 15,
+			},
+			0: {
+				items: 1,
+			}
+		}
+	});
+
+	$(document).on('click', '#section-appscreen2 .owl-item>div', function () {
+		$owl.trigger('to.owl.carousel', $(this).data('position'));
+	});
+
 })(jQuery);
